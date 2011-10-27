9db76a4ae90465a098991d78ca5fc5ba23ddf803
dean-silversquare <dean-silversquare@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-10-27 20:27:24 +0000

    Updated to grab featured image and any images in post if there isn't a specified image URL to use. Updated to generate a description tag based on a post's caption or content. Segmented settings code to only be called in the admin menu
    git-svn-id: https://plugins.svn.wordpress.org/fb-thumbnail-config/trunk@456483 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- FB_Thumbnail_Config.php
+++ FB_Thumbnail_Config.php
@@ -11,69 +11,58 @@
  * @license GNU General Public License
  */
 
-/**
- * Include Rilwsi's Meta Boxes
- */
-// get path to plugin
-include( 'meta-box.php' );
+function createAdminMenu()
+{
+	/**
+	 * Include Rilwsi's Meta Boxes
+	 */
+	// get path to plugin
+	include( 'meta-box.php' );
 
-/**
- * Set up 100 by 100 thumbnail size for fb
- */
-if ( function_exists( 'add_theme_support' ) ) 
-	add_theme_support( 'post-thumbnails' );
-if ( function_exists( 'add_image_size' ) )
-	add_image_size( 'fb-thumb', 100, 100 );
-
-/** 
- * BEGIN Meta Boxes
- * 
- * Uses Rilwis's Meta Boxes 
- * (http://www.deluxeblogtips.com/2010/04/how-to-create-meta-box-wordpress-post.html) 
- */
-$meta_boxes = array();
-$prefix = 'fbtc_';
-$meta_boxes[] = array(
-	'id' => 'fb_thumbmail_config_details',
-	'title' => 'Fb Thumbnail Configuration',
-	'pages' => array('post','page'),
-	'context' => 'normal', // normal, advanced, side (optional)
-	'priority' => 'low',
-	'fields' => array(
-		array(
-			'name' => 'Add Image:',
-			'desc' => 'Copy and paste the url to the image you want to use here. For best results use a 100 by 100 pixel image.',
-			'id' => $prefix . 'the_thumb',
-			'type' => 'text'
-		),
-		array(
-			'name' => 'Use Thumbnail:',
-			'desc' => 'Check to use the thubmnail you entered above as the Facebook thumbnail (leave unchecked to let Facebook auto-generate).',
-			'id' => $prefix . 'use_thumb',
-			'type' => 'checkbox'
-		),
-		array(
-			'name' => 'Description:',
-			'desc' => 'Enter the description shown on Facebook. Keep your description under 43 words - anything over will be ignored.',
-			'id' => $prefix . 'the_desc',
-			'type' => 'textarea'
-		),
-		array(
-			'name' => 'Use Description:',
-			'desc' => 'Check to use the description you entered above as the Facebook description (leave unchecked to let Facebook auto-generate).',
-			'id' => $prefix . 'use_desc',
-			'type' => 'checkbox'
+	/** 
+	 * BEGIN Meta Boxes
+	 * 
+	 * Uses Rilwis's Meta Boxes 
+	 * (http://www.deluxeblogtips.com/2010/04/how-to-create-meta-box-wordpress-post.html) 
+	 */
+	$meta_boxes = array();
+	$prefix = 'fbtc_';
+	$meta_boxes[] = array(
+		'id' => 'fb_thumbmail_config_details',
+		'title' => 'Fb Thumbnail Configuration',
+		'pages' => array('post','page'),
+		'context' => 'normal', // normal, advanced, side (optional)
+		'priority' => 'low',
+		'fields' => array(
+			array(
+				'name' => 'Add Image:',
+				'desc' => 'Copy and paste the url to the image you want to use here. For best results use a 100 by 100 pixel image.',
+				'id' => $prefix . 'the_thumb',
+				'type' => 'text'
+			),
+			array(
+				'name' => 'Use Thumbnail:',
+				'desc' => 'Check to use the thubmnail you entered above as the Facebook thumbnail (leave unchecked to let Facebook auto-generate).',
+				'id' => $prefix . 'use_thumb',
+				'type' => 'checkbox'
+			),
+			array(
+				'name' => 'Generate Description:',
+				'desc' => 'Check to generate a description based on your content.',
+				'id' => $prefix . 'use_desc',
+				'type' => 'checkbox'
+			)
 		)
-	)
-);
+	);
 
-// create the meta boxes 
-foreach ($meta_boxes as $meta_box)
-	$my_box = new RW_Meta_Box($meta_box);
-/** 
- * END Meta Boxes
- */
-
+	// create the meta boxes 
+	foreach ($meta_boxes as $meta_box)
+		$my_box = new RW_Meta_Box($meta_box);
+	/** 
+	 * END Meta Boxes
+	 */
+}
+add_action ( 'admin_init', 'createAdminMenu');
 
 /**
  *  Add meta data Facebook is looking for to head of post/page
@@ -85,23 +74,36 @@ function fb_thumbnail_config_header_action()
 	$use_thumb = get_post_meta($post->ID, $prefix.'use_thumb', true);
 	$the_thumb = get_post_meta($post->ID, $prefix.'the_thumb',true );
 	$use_desc = get_post_meta($post->ID, $prefix.'use_desc', true);
-	$the_desc = get_post_meta($post->ID, $prefix.'the_desc',true );
 	
 	// add meta for type, url, and title
 	if($use_thumb || $use_desc):
-	?><meta property="og:type" content="article" />
+	?><meta property="og:site_name" content="<?php bloginfo('name'); ?>" />
+<meta property="og:type" content="article" />
 <meta property="og:url" content="http://<?php echo $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]; ?>" />
 <meta property="og:title" content="<?php wp_title('|', 1, 'right'); bloginfo('name'); ?>"/>
 
 	<?php endif;
-
+	
+	$description = strip_tags ( $thispost->post_excerpt ? $thispost->post_excerpt : $thispost->post_content );
 	// add meta for description
-	if($use_desc && $the_desc)
-		echo "<meta property=\"og:description\" content=\"$the_desc\"/>\n";
-
+	if($use_desc)
+		echo "<meta property=\"og:description\" content=\"$description\"/>\n";
+	
 	// add meta for image
-	if($use_thumb) {
+	if($use_thumb)
+	{
 		echo "<meta property=\"og:image\" content=\"$the_thumb\"/>\n";
+	} else {
+		if ( function_exists ( 'has_post_thumbnail' ) AND has_post_thumbnail($post->ID) )
+		{
+				$attachment = wp_get_attachment_image_src ( get_post_thumbnail_id($thispost->ID) );
+				$image = $attachment[0];
+		}
+		elseif ( preg_match ( '/<img\s[^>]*src=["\']?([^>"\']+)/i', $thispost->post_content, $match ) ) 
+		{
+			$image = $match[1];
+		}
+		echo "<meta property=\"og:image\" content=\"$image\"/>\n";
 	}
 }
 add_action('wp_head', 'fb_thumbnail_config_header_action');
